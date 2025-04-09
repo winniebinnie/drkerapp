2e27b85914bb2578d132d0f0444fc612c07d18d6
winnieBinnie <ritthanupahp_s@cmu.ac.th>
2025-04-09 09:07:38 +0700

    Added Placeholders

---
+++ lib/screens/blog_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BlogScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("Latest Blog Posts"));
+  }
+}
